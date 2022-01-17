c3dcb33b354a5a203f9b3de06a1a27b0bbb60e2a
joaobarros87 <jcoding@protonmail.com>
2022-01-17 19:26:20 -0300

    Class about best practices in Java.

---
+++ GFTJava/vDM/src/dio/com/br/vDM.java
@@ -0,0 +1,43 @@
+package dio.com.br;
+
+public class vDM {
+
+    public static void main (String[] args) {
+
+        int i;
+       // int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        int asrn2467md = 10;
+        //int asrn246 78md;
+        int asrn2$2467_md = 10;
+        //int asrn2$46%78_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+        System.out.println(j);
+        System.out.println(asrn2467md);
+        System.out.println(asrn2$2467_md);
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+    }
+}
