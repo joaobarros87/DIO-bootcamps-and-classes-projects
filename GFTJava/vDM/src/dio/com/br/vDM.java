122ce2797be3ad96865eba0a578a088c7b5b0ce0
joaobarros87 <jcoding@protonmail.com>
2022-01-17 21:54:31 -0300

    Added Data Types and Operators exercises.

--- GFTJava/vDM/src/dio/com/br/vDM.java
+++ GFTJava/vDM/src/dio/com/br/vDM.java
@@ -4,10 +4,12 @@ public class vDM {
 
     public static void main (String[] args) {
 
+// Best Practices
+
         int i;
-       // int i;
+       // int i; I've already a i variable
         int I;
-        //int 1a;
+        //int 1a; Never use numbers
         int _1a;
         int $aq;
 
@@ -18,16 +20,16 @@ public class vDM {
 
         final int j = 10;
         int asrn2467md = 10;
-        //int asrn246 78md;
+        //int asrn246 78md; Never use spaces
         int asrn2$2467_md = 10;
-        //int asrn2$46%78_md = 10;
+        //int asrn2$46%78_md = 10; Never use %
 
         int quantidadeProduto = 50;
-        //int QuantidadeProduto;
+        //int QuantidadeProduto; Not using cameCase
         final int NUMERO_TENTATIVAS = 5;
-        //final int numeroTentativas = 5;
+        //final int numeroTentativas = 5; Never use spaces
         int QUANTIDADE_OPCOES = 25;
-        //int qtdProd;
+        //int qtdProd; Not specific
 
         System.out.println(i);
         System.out.println(I);
@@ -39,5 +41,92 @@ public class vDM {
         System.out.println(quantidadeProduto);
         System.out.println(NUMERO_TENTATIVAS);
         System.out.println(QUANTIDADE_OPCOES);
+
+        // Data Types
+
+        byte c = -127;
+        byte d = 80;
+
+        short f = -27640;
+        short g = 26597;
+
+        int a = -445754;
+        int b = 6465776;
+
+        long h = 987565558423L;
+        long k = -8564688721646L;
+
+        float m = 3.14145756f;
+        float n = -2.9497598756f;
+
+        double o = -1987621445789178164d;
+        double p = 98516479798258647954d;
+
+        char q = 'Q';
+        String r = "Hello, r!";
+
+
+        boolean s = true;
+        boolean t = false;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(h);
+        System.out.println(k);
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(o);
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(r);
+        System.out.println(s);
+        System.out.println(t);
+
+// Operators
+
+        int ab = 2;
+        int ba = 3;
+        int xis = 100;
+        int al = ab++;
+        int ao = --ba;
+
+
+        float ac = 7.3f/2.2f;
+        long ad = 121148L*255465L;
+        double ae = 345605489d/4756d;
+        int af = 100%10;
+        int ag = ab+=5;
+        int ah = ab-=6;
+        int ai = xis/=ab;
+        int aj = al*=xis;
+        int ak = ao%=2;
+        int ap = ++ab + ba-- * ab / ba;
+
+
+
+
+
+
+
+        System.out.println(al);
+        System.out.println(ao);
+        System.out.println(ac);
+        System.out.println(ad);
+        System.out.println(ae);
+        System.out.println(af);
+        System.out.println(ag);
+        System.out.println(ah);
+        System.out.println(ai);
+        System.out.println(aj);
+        System.out.println(ak);
+        System.out.println(ap);
+
+
+
+
     }
 }
