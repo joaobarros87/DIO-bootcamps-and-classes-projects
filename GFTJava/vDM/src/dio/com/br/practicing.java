1d4544f2630718c60fd51f0046eff323f7b2ad96
joaobarros87 <jcoding@protonmail.com>
2022-01-18 17:30:17 -0300

    Pacticing operators.

---
+++ GFTJava/vDM/src/dio/com/br/practicing.java
@@ -0,0 +1,24 @@
+package dio.com.br;
+
+public class practicing {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 2;
+        int c = 4;
+
+        int d = ++a;
+        int e = 2 + d + (c * 3) / 2 + b;
+
+        int f = e - 13;
+        int g = f * (10/a);
+
+        System.out.println(e);
+        System.out.println("f = " + f);
+        System.out.println(g);
+
+
+    }
+
+
+}
