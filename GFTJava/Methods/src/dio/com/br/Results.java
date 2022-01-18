2b53172c709d2efbbb1c173c90c1988551351039
joaobarros87 <jcoding@protonmail.com>
2022-01-18 18:37:29 -0300

    Clealing project

---
+++ GFTJava/Methods/src/dio/com/br/Results.java
@@ -0,0 +1,16 @@
+package dio.com.br;
+
+public class Results {
+    public static void main(String[] args) {
+
+        System.out.println("methodCalc Results");
+        System.out.println("__________________");
+        methodCalc.sum(2,5);
+        methodCalc.subtract(15,2.6);
+        methodCalc.multiply(3,7.5);
+        methodCalc.divide(30, 10);
+
+    }
+
+
+}
---
+++ GFTJava/Methods/src/dio/com/br/methodCalc.java
@@ -0,0 +1,38 @@
+package dio.com.br;
+
+public class methodCalc {
+    public static void sum(double number1, double number2) {
+
+        double result = number1 + number2;
+
+        System.out.println("The result of " +  number1 + " plus " +  number2 + " is: " + result);
+
+    }
+
+    public static void subtract(double number1, double number2){
+
+        double result = number1 - number2;
+
+        System.out.println("The result of " +  number1 + " minus " +  number2 + " is: " + result);
+    }
+
+    public static void multiply(double number1, double number2){
+
+        double result = number1 * number2;
+
+        System.out.println("The result of " +  number1 + " times " +  number2 + " is: " + result);
+    }
+
+    public  static void divide(double number1, double number2){
+
+        double result = number1 / number2;
+
+        System.out.println("The result of " +  number1 + " divided by " +  number2 + " is: " + result);
+    }
+
+
+
+
+
+
+}
